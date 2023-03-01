ab2a2dd2776bf77866639e51339c067bcc4f3fe4
bin.liu <bin.liu@freemud.com>
2023-03-01 18:04:14 +0800

    完成侧边栏

--- web/src/layout/Layout.vue
+++ web/src/layout/Layout.vue
@@ -33,7 +33,54 @@
       <!-- 中部  68px是header的高度 -->
       <a-layout style="height:calc(100vh - 68px)">
         <!-- 侧边栏 -->
-        <a-layout-sider width="240px"  v-model:collapsed="collapsed" collapsible></a-layout-sider>
+        <a-layout-sider width="240px"  v-model:collapsed="collapsed" collapsible>
+             <a-menu
+             :selectedKeys="selectedKeys2"
+             :openKeys="openKeys"
+             @openChange="onOpenChange"
+             mode="inline"
+             :style="{height: '100%',boderRight: 0}"
+             >
+              <!-- routers的信息就是router/index.js下面的路由信息 -->
+              <template v-for="menu in routers" :key="menu">
+                <!-- 处理无子路由的情况 -->
+                 <a-menu-item
+                   v-if="menu.children && menu.children.length === 1"
+                   :index="menu.children[0].path"
+                   :key="menu.children[0].path"
+                   @click="routeChange('item', menu.children[0].path)"
+                 >
+                  <template #icon>
+                    <component :is="menu.children[0].icon"></component>
+                  </template>
+                  <span>{{ menu.children[0].name }}</span>
+                 </a-menu-item>
+                 <!-- 处理有子路由的情况 -->
+                 <a-sub-menu
+                  v-else-if="menu.children && menu.children.length > 1"
+                  :index="menu.path"
+                  :key="menu.path"
+                 >
+                   <template #icon>
+                      <component :is="menu.icon"></component>
+                    </template>
+                    <template #title>
+                      <span>
+                          <span :class="[collapsed ? 'is-collapse':'']">{{ menu.name }}</span>
+                      </span>
+                    </template>
+                    <!-- 子路由 -->
+                    <a-menu-item 
+                        v-for="child in menu.children" 
+                        :key="child.path" 
+                        :index="child.path" 
+                        @click="routeChange('sub', child.path)">
+                        <span>{{ child.name }}</span>
+                    </a-menu-item>
+                 </a-sub-menu>
+              </template>
+             </a-menu>
+        </a-layout-sider>
 
         <!-- main -->
         <a-layout style="padding: 0 24px">
@@ -53,7 +100,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import kubeLog from '@/assets/k8s-metrics.png'
 import avator from '@/assets/avator.png'
 import { useRouter } from 'vue-router';
@@ -61,8 +108,39 @@ import { useRouter } from 'vue-router';
 const collapsed = ref(false)
 const selectedKeys1 = ref([])
 const clusterList = ref(["TEST1", "TEST2"])
-
+// 获取router
 const router = useRouter()
+// 路由信息
+const routers = ref([])
+const selectedKeys2 = ref([])
+const openKeys = ref([])
+
+// 切换路由
+const routeChange = (type, path) => {
+    // 如果是单独的栏目
+    if (type != 'sub'){
+        openKeys.value = []
+    }
+
+    // 选中当前path对应
+    selectedKeys2.value = [path]
+
+    // 页面跳转
+    if (router.currentRoute.value.path !== path){
+        router.push(path)
+    }
+}
+
+// 从浏览器地址直接打开后的选择
+function DirectOpen(value) {
+    selectedKeys2.value = [value[1].path]
+    openKeys.value = [value[0].path]
+}
+
+const onOpenChange = (value) => {
+    const latestOpenKey = value.find(key => openKeys.value.indexOf(key) === -1)
+    openKeys.value = latestOpenKey ? [latestOpenKey]:[] 
+}
 
 const logout = () => {
     // 移除用户信息
@@ -70,6 +148,12 @@ const logout = () => {
     localStorage.removeItem('token')
     router.push('/login')
 }
+
+onMounted(() => {
+    routers.value = router.options.routes
+
+    DirectOpen(router.currentRoute.value.matched)
+})
 </script>
 
 <style  scoped>
@@ -80,5 +164,9 @@ const logout = () => {
 
 .ant-layout-header {
     padding: 0 30px !important;
+}
+
+.is-collapse{
+    display: none;
 }
 </style>
