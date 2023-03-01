16bcf000290b0f1bff4dfde2e03604cbc1feca4f
bin.liu <bin.liu@freemud.com>
2023-03-01 13:59:16 +0800

    完成导航栏

--- web/src/layout/Layout.vue
+++ web/src/layout/Layout.vue
@@ -3,7 +3,31 @@
       <!-- 固钉 -->
       <a-affix>
            <!-- 头部 -->
-          <a-layout-header></a-layout-header>
+          <a-layout-header>
+            <!-- 平台信息 -->
+            <div style="float: left;">
+                <img style="height: 40px" :src="kubeLog">
+                <span style="font-size: 16px; padding: 0 10px 0 10px; font-weight: bold;">k8sDashboard</span>
+            </div>
+            <!-- 集群信息 -->
+            <a-menu style="float: left; width: 250px;" v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal">
+              <a-menu-item v-for="item in clusterList" :key="item">{{ item }}</a-menu-item>
+            </a-menu>
+            <!-- 用户信息 -->
+            <div style="float: right;">
+              <img :src="avator" style="height: 40px; border-radius: 50%; margin-right: 10px">
+              <a-dropdown :overlayStyle="{paddingTop: '20px'}">
+                <a >Admin <down-outlined /></a>
+                <template #overlay>
+                    <a-menu>
+                      <a-menu-item><a @click="logout">退出登陆</a></a-menu-item>
+                      <a-menu-item><a >修改密码</a></a-menu-item>
+                    </a-menu>
+                </template>
+              </a-dropdown>
+            </div>
+
+          </a-layout-header>
       </a-affix>
 
       <!-- 中部  68px是header的高度 -->
@@ -29,16 +53,32 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
+import kubeLog from '@/assets/k8s-metrics.png'
+import avator from '@/assets/avator.png'
+import { useRouter } from 'vue-router';
 
 const collapsed = ref(false)
+const selectedKeys1 = ref([])
+const clusterList = ref(["TEST1", "TEST2"])
 
+const router = useRouter()
 
+const logout = () => {
+    // 移除用户信息
+    localStorage.removeItem('username')
+    localStorage.removeItem('token')
+    router.push('/login')
+}
 </script>
 
 <style  scoped>
 .ant-layout-footer {
     padding: 5px 50px  !important;
     color: rgb(239, 239, 239);
+}
+
+.ant-layout-header {
+    padding: 0 30px !important;
 }
 </style>
